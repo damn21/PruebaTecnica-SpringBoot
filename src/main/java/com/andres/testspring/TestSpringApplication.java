b9fb2033555dbba82c2d487279b3c8f54e20dc5d
Andres Escorcia <andres.escorcia@patagoniansys.com>
2023-01-13 14:09:53 -0500

    instalacion de H2

--- src/main/java/com/andres/testspring/TestSpringApplication.java
+++ src/main/java/com/andres/testspring/TestSpringApplication.java
@@ -1,13 +1,19 @@
 package com.andres.testspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestSpringApplication {
+public class TestSpringApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSpringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//Creacion de tablas H2
+
+	}
 }
